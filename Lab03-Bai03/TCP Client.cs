196dfa256a3add53ae2555283ad270baa655749f
ThaiNhatThu <21522648@gm.uit.edu.vn>
2023-04-23 13:39:28 +0700

    meow meow

---
+++ Lab03-Bai03/TCP Client.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Lab03_Bai03
+{
+    public partial class TCP_Client : Form
+    {
+        private TcpClient client;
+        public TCP_Client()
+        {
+            InitializeComponent();
+        }
+
+        //Ngắt kết nối 
+        private void DisconnectButton_Click(object sender, EventArgs e)
+        {
+            if (client != null && client.Connected)
+            {
+                client.Close();
+                MessageBox.Show("Disconnected from server", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                richTextBox1.Clear();
+            }
+        }
+
+        //Kết nối tới server 
+        private void ConnectButton_Click(object sender, EventArgs e) //
+        {
+            client = new TcpClient();
+            try
+            {
+                client.Connect(IPAddress.Loopback, 8080);
+                MessageBox.Show("Connected to server","OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error connecting to server: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {   
+            //Kiểm tra kết nối 
+            if (client == null || !client.Connected)
+            {
+                MessageBox.Show("Not connected to server", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //Lẩy nội dung từ richTextBox1 chuyển sang mảng byte và gửi đến server bằng networkstream của client 
+            string message = richTextBox1.Text;
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+                MessageBox.Show("Message sent");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error sending message: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
