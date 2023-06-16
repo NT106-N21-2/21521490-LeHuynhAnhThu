bbbf389b23b0f2535500b2094c4448eea833ecc5
Anh Thu <anhthu@github.com>
2023-06-16 07:57:08 +0700

    newww

---
+++ Lab06/Lab06.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Lab06
+{
+    partial class Lab06
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(242, 182, 187);
+            button3.BackgroundImageLayout = ImageLayout.Center;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(598, 184);
+            button3.Name = "button3";
+            button3.Size = new Size(181, 83);
+            button3.TabIndex = 11;
+            button3.Text = "BÀI 03";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(242, 182, 187);
+            button2.BackgroundImageLayout = ImageLayout.Center;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(307, 184);
+            button2.Name = "button2";
+            button2.Size = new Size(191, 83);
+            button2.TabIndex = 10;
+            button2.Text = "BÀI 02";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(242, 182, 187);
+            button1.BackgroundImageLayout = ImageLayout.Center;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(21, 184);
+            button1.Name = "button1";
+            button1.Size = new Size(181, 83);
+            button1.TabIndex = 9;
+            button1.Text = "BÀI 01";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Lab06
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Lab06";
+            Text = "Lab06";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button3;
+        private Button button2;
+        private Button button1;
+    }
+}
---
+++ Lab06/Lab06_Bai01.cs
@@ -0,0 +1,10 @@
+namespace Lab06
+{
+    public partial class Lab06_Bai01 : Form
+    {
+        public Lab06_Bai01()
+        {
+            InitializeComponent();
+        }
+    }
+}
